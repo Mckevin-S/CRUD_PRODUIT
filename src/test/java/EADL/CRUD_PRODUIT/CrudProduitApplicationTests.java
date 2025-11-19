2e6c872a8c70484f09d5b225acf34b11e6cb2b0a
Mckevin-S <kamguemmckevin@gmail.com>
2025-11-19 06:48:11 +0100

    initialisation du projet

---
+++ src/test/java/EADL/CRUD_PRODUIT/CrudProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package EADL.CRUD_PRODUIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
